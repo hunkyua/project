694ef25200cf4d90a9cef8d136ed05a9ff710c6a
hunkyua <hunkyua@users.noreply.github.com>
2015-06-15 19:08:19 +0300

    Update StartAnagramMaker.java

--- StartAnagramMaker.java
+++ StartAnagramMaker.java
@@ -1,25 +1,20 @@
 package ru.yandex.op.ua;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 
 public class StartAnagramMaker {
 
-	public static void main(String[] args) throws IOException {
-	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	String str = reader.readLine();
-	
+	public static void main(String[] args) {
+	String str = "а1бвгд ежз!и";
 	AnagramMaker stroka = new AnagramMaker();
 	String reversed = stroka.getReversed(str);
 	System.out.println(reversed);
 	AnagramMaker anagrammer = new AnagramMaker();
 	
-	if (anagrammer.getReversed("а1бвгд ежз!и").equals("д1гвба изж!е"))
+	if (anagrammer.getReversed("а1бвгд ежз!и").equals(reversed))
 		System.out.println("passed");
 	else  	
 		System.out.println("failed");
 	
 	}
+	
 }
