02576f0786348127cb2a14b7c1c56e98a70969b0
hunkyua <hunkyua@users.noreply.github.com>
2015-06-16 10:35:45 +0300

    Update StartAnagramMaker.java

--- StartAnagramMaker.java
+++ StartAnagramMaker.java
@@ -1,6 +1,5 @@
 package ru.yandex.op.ua;
 
-
 public class StartAnagramMaker {
 
 	public static void main(String[] args) {
@@ -10,7 +9,7 @@ public class StartAnagramMaker {
 	System.out.println(reversed);
 	AnagramMaker anagrammer = new AnagramMaker();
 	
-	if (anagrammer.getReversed("а1бвгд ежз!и").equals(reversed))
+	if (anagrammer.getReversed("а1бвгд ежз!и").equals("д1гвба изж!е"))
 		System.out.println("passed");
 	else  	
 		System.out.println("failed");
