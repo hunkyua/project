14e4ccff9d3cd5cfe390df5d1b6a422570780753
hunkyua <hunkyua@users.noreply.github.com>
2015-06-23 13:11:03 +0300

    Update and rename StartDevision.java to StartDivision.java

--- StartDivision.java
+++ StartDivision.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class StartDevision {
+public class StartDivision {
     public static void main(String[] args) throws IOException {
         
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
