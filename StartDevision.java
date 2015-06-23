02bf7a34f1593ec1bb2e2be5fe5b6d4d89970541
hunkyua <hunkyua@users.noreply.github.com>
2015-06-23 13:09:20 +0300

    Create StartDevision.java

---
+++ StartDevision.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class StartDevision {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите делимое: ");
+        String division=buffer.readLine();
+        System.out.println("Введите делитель: ");
+        String secondParam=buffer.readLine();
+        int divider=Integer.valueOf(secondParam);
+
+
+        Division example = new Division();
+        example.printDivision(division, divider);
+
+
+    }
+}
