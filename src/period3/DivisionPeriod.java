3d70e6785a7cdc15b922b30b6685a9295d43ed0d
hunkyua <op.ua@yandex.ru>
2015-07-24 13:30:37 +0300

    change

--- src/period3/DivisionPeriod.java
+++ src/period3/DivisionPeriod.java
@@ -1,8 +1,8 @@
 package period3;
 
-        import java.util.ArrayList;
-        import java.util.LinkedList;
-        import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class DivisionPeriod {
 
