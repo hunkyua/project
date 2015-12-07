7b891c989eea5cd492a0161c67ed2c10ede75f2a
hunkyua <op.ua@yandex.ru>
2015-12-07 09:45:06 +0200

    add akka

--- Akka/src/main/java/ClientActor.java
+++ Akka/src/main/java/ClientActor.java
@@ -3,9 +3,7 @@ import akka.actor.Props;
 import akka.actor.UntypedActor;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Collectors;
+
 
 /**
  * Created by Hunky on 06.12.2015.
@@ -21,13 +19,6 @@ public class ClientActor extends UntypedActor {
         String line = null;
         while ((line = br.readLine()) != null) {
             worker.tell(line, getSelf());
-        }
-        try (PrintWriter out = new PrintWriter(new File("1000.txt"))) {
-            Files.lines(Paths.get("c:/100000.txt"))
-                    .map(s -> s.split(";"))
-                    .collect(Collectors.groupingBy(a -> a[0],
-                            Collectors.summingInt(a -> Integer.parseInt(a[1]))))
-                    .forEach((k,v) -> out.println(k + ";" + v));
         }
         br.close();
     }
